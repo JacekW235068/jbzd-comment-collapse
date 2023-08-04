c7798e3c361204ce1a571d3883f938a10929e6ce
Jacek Wierzowiecki <cubeMan217@gmail.com>
2023-08-04 16:41:00 +0200

    Exponential slider for animation speed

--- options.js
+++ options.js
@@ -1,8 +1,22 @@
+
+// the trick is that slider has 0-100 range (100/10)^2 = 100
+expoSliderValue = (v) => {
+  x = Math.pow(parseFloat(v)/10, 2.0)
+  x = x*10.0 // to milisecs
+  return parseInt(x)
+}
+
+valueSliderExpo = (x) => {
+  x = x / 10.0
+  v = Math.sqrt(x)
+  return v*10
+}
+
 function saveOptions(e) {
     browser.storage.sync.set({
       fade: document.querySelector("#fade").checked,
       collapseText: document.querySelector("#collapse").checked,
-      animationSpeed: document.querySelector("#speed").value
+      animationSpeed: expoSliderValue(parseFloat(document.querySelector("#speed").value))
     });
     e.preventDefault();
   }
@@ -16,10 +30,10 @@ function saveOptions(e) {
       document.getElementById('fade').checked = items.fade;
       document.getElementById('collapse').checked = items.collapseText;
       e = document.getElementById("speed");
-      e.value = items.animationSpeed
-      document.getElementById('current-speed').innerText = `${(parseFloat(items.animationSpeed)/100.0).toFixed(3)}`;
+      e.value = valueSliderExpo(items.animationSpeed)
+      document.getElementById('current-speed').innerText = `${items.animationSpeed}`;
       e.addEventListener('change' , (e) => {
-        document.getElementById('current-speed').innerText = `${(parseFloat(e.currentTarget.value)/100.0).toFixed(3)}`;
+        document.getElementById('current-speed').innerText = `${expoSliderValue(parseFloat(e.currentTarget.value))}`;
       })
     })
   }
